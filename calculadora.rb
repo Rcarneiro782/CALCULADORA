64b9b315c6dc601c767c22e6d809e3bff2ea8984
Rafael Carneiro <rcarneiro782@gmail.com>
2024-05-22 11:15:54 -0300

    Menu Calculadora

---
+++ calculadora.rb
@@ -0,0 +1,17 @@
+Resultado = ''
+
+puts 'Escolha uma opção'
+puts '1- Soma.'
+puts '2- Subtração. '
+puts '3- Multiplicação. '
+puts '4- Divisão. '
+puts '0- Sair'
+
+print 'Escolha a sua Opção'
+opcao = gets.chomp.to_i
+
+break if opcao == 0
+
+
+
+
